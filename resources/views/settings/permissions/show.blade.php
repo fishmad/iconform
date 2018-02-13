adc31302b0d358c27d63aeb2dec482ac2206092e
Nielo <fishmadwebdesign@gmail.com>
2018-02-13 15:43:07 +1100

    issues for tissues

--- resources/views/settings/permissions/show.blade.php
+++ resources/views/settings/permissions/show.blade.php
@@ -1,56 +1,45 @@
-@extends('admin.layouts.master')
-
+@extends('_layouts.master')
 @section('content')
-    <div class="container">
-        <div class="row">
-            @include('admin.partials.sidebar')
 
-            <div class="col-md-9">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-md-6">
-                              Permission: {{ $permission->name }}
-                            </div>
-                            <div class="col-md-6 btn-toolbar">
-                                <a href="{{ url('/admin/permissions') }}" class="btn btn-xs btn-default pull-right" title="Back"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</a>
-                                <a href="{{ url('/admin/permissions/' . $permission->id . '/edit') }}" class="btn btn-xs btn-default pull-right" title="Edit Permission"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>                       
-                                <div class="btn-group pull-right">
-                                    {!! Form::open([
-                                        'method'=>'DELETE',
-                                        'url' => ['admin/permissions/', $permission->id]
-                                        ,
-                                        {{--  'style' => 'display:inline'  --}}
-                                    ]) !!}
-                                        {!! Form::button('<i class="fa fa-trash-o" aria-hidden="true"></i> Delete', array(
-                                            'type' => 'submit',
-                                            'class' => 'btn btn-xs btn-default pull-right',
-                                            'title' => 'Delete Permission',
-                                            'onclick'=>'return confirm("Confirm delete?")'
-                                        ))!!}
-                                    {!! Form::close() !!}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="panel-body">
-                        <div class="table-responsive">
-                            <table class="table table-borderless">
-                                <tbody>
-                                    <tr>
-                                        <th>ID</th>
-																				<td>{{ $permission->id }}</td>
-                                    </tr>
-                                    <tr>
-																				<th>Name</th>
-																				<td>{{ $permission->name }}</td>
-																		</tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+      <div class="container-fluid">
+        <div class="animate fadeIn">
+          <div class="card">
+            <div class="card-header">Permission: {{ $permission->name }}
+              <div class="card-actions">
+                <a href="#" class="btn-minimize"><i class="icon-arrow-down"></i></a>
+                <a href="#" class="btn-close"><i class="icon-close"></i></a>
+              </div>
             </div>
-        </div>
-    </div>
+            <div class="card-body">
+              <div class="col-md-12">
+                <a href="{{ url('/settings/permissions') }}" class="btn btn-secondary" title="Back"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</a>
+                <a href="{{ url('/settings/permissions/' . $permission->id . '/edit') }}" class="btn btn-secondary" title="Edit Permission"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</a>                       
+                  {!! Form::open([
+                    'method'=>'DELETE',
+                    'url' => ['settings/permissions/', $permission->id],
+                  ]) !!}
+                  {!! Form::button('<i class="fa fa-trash-o" aria-hidden="true"></i> Delete', array(
+                    'type' => 'submit',
+                    'class' => 'btn btn-secondary',
+                    'title' => 'Delete Permission',
+                    'onclick'=>'return confirm("Confirm delete?")'
+                  ))!!}
+                    {!! Form::close() !!}
+                </div>
+              <table class="table table-borderless">
+                <tbody>
+                  <tr>
+                    <th>ID</th>
+                    <td>{{ $permission->id }}</td>
+                  </tr>
+                  <tr>
+                    <th>Name</th>
+                    <td>{{ $permission->name }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div><!-- /.card-body -->
+          </div><!-- /.card -->
+        </div><!-- /.animate.fadeIn -->
+      </div><!-- /.container-fluid -->
 @endsection
