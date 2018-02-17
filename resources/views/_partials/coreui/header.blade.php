15099cc73b9a58ad6be2afb118b49c340472de5e
Fishmad <fishmadwebdesign@gmail.com>
2018-02-17 18:37:01 +1100

    Breadcrumb3

--- resources/views/_partials/coreui/header.blade.php
+++ resources/views/_partials/coreui/header.blade.php
@@ -13,8 +13,8 @@
       </li>
     </ul>  --}}
 
-    @include('_partials.coreui.breadcrumbs-header')
-    
+    @include('_partials.coreui.breadcrumbsheader')
+
 
     <ul class="nav navbar-nav ml-auto">
       <li class="nav-item d-md-down-none"><a class="nav-link" href="#"><i class="icon-bell"></i><span class="badge badge-pill badge-danger">5</span></a></li>
